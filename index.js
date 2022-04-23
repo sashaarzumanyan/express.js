11fcbb7a175441b25599bde9414c12dd4a0492e0
sashaarzumanyan <sashaarzumanyan25@gmail.com>
2022-04-23 15:44:26 +0400

    make it better

--- index.js
+++ index.js
@@ -26,7 +26,7 @@ const start = async () => {
     try {
         await sequelize.authenticate()
         await sequelize.sync()
-        app.listen(PORT, () => console.log(`Server started on port ${5000}`))
+        app.listen(PORT, () => console.log(`Server started on port ${PORT}`))
     } catch (e) {
         console.log(e)
     }
